0744654a31c1b6280e9e3dc4719bf90233c74839
liuxuesi <lxs@kuaihuoyun.com>
2018-03-06 15:57:42 +0800

    优化

--- reverse-proxy/src/main/java/cn/demo/reverseProxy/Application.java
+++ reverse-proxy/src/main/java/cn/demo/reverseProxy/Application.java
@@ -1,9 +1,12 @@
 package cn.demo.reverseProxy;
 
+import cn.demo.reverseProxy.filter.AccessPasswordFilter;
+import cn.demo.reverseProxy.filter.AccessUserNameFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @Author: Leo
@@ -18,4 +21,19 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Bean
+    public AccessUserNameFilter accessUserNameFilter() {
+        return new AccessUserNameFilter();
+    }
+
+    @Bean
+    public AccessPasswordFilter accessPasswordFilter() {
+        return new AccessPasswordFilter();
+    }
+
+    //@Bean
+    //public AccessTokenFilter accessTokenFilter() {
+    //    return new AccessTokenFilter();
+    //}
 }
